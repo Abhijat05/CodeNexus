ff120e3a5930908255437b132b7a17821f814a31
Abhijat05 <abhijatsinha2004@gmail.com>
2025-02-09 17:01:53 +0530

    implement user profile and logout functionality with Redis integration

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
+import redisClient from "../services/redis.service.js";
 
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
@@ -38,5 +39,24 @@ export const loginController = async (req, res) => {
     res.status(200).json({ user, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
+  }
+};
+
+export const profileController = async (req, res) => {
+
+  res.status(200).json({
+    user: req.user,
+  });
+};
+
+export const logoutController = async (req, res) => {
+  try {
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+    redisClient.SET(token, "logout", "EX", 60 * 60 * 24);
+    res.send(200).json({
+      message: "Logout successfully",
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
   }
 };
